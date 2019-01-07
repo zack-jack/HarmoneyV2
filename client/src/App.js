391a912b65df692f634643ac0e73fa52c11cbb78
Zack Jackson <z.jackson58@gmail.com>
2019-01-06 20:33:36 -0500

    React router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
+import Register from './components/Register';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>App</h1>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/user/register" component={Register} />
+          <Route path="/user/register" component={Login} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
